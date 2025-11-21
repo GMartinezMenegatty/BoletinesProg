db995f45a6c4b4983ddb3a1f09a042055ff0ae50
Gabriela <gmartinezmenegatty@danielcastelao.org>
2025-11-21 10:18:02 +0100

    Añadido cambios

--- Clases/main.py
+++ Clases/main.py
@@ -1,6 +1,7 @@
 from punto import Punto
 from circulo import Circulo
 from cilindro import Cilindro
+from data import Data
 
 p1 = Punto(2,3)
 p2 = Punto(9,1)
@@ -26,3 +27,7 @@ print(c1.aCadea())
 
 cil = Cilindro (3,2,3, 5)
 print(cil.calcularArea())
+
+hoxe = Data (21, 11, 2025)
+
+print (hoxe)
