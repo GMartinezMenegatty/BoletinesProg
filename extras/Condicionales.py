1e31a8d277c7cb6e517b6c416b4af5f85735d677
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-01-22 13:21:47 +0100

    Condicionales

---
+++ extras/Condicionales.py
@@ -0,0 +1,46 @@
+# Condicional multiple match case
+
+x = 40
+
+if x == 10:
+    print ("X e 10")
+elif x == 20:
+    print("X e 20")
+elif x == 30:
+    print("X e 30")
+
+match x:
+    case 10:
+        print ("X e 10")
+    case 20:
+        print ("X e 20")
+    case 30:
+        print ("X e 30")
+    case _:
+        print("X non e 10, 20 ou 30")
+
+match x:
+    case 10|20|30:
+        print (f"X e {x}")
+    case _:
+        print("X non e 10, 20 ou 30")
+
+y = 20
+match x:
+    case 10 if y % 2 == 0:
+        print("X e 10 e Y divisible entre 2")
+    case 10:
+        print("X e 10 e Y non divisible entre 2")
+    case _:
+        print ("Opcion desconocida")
+
+
+l = [2, 3, 5]
+
+match l:
+    case (x, y):
+        print (f"Coleccion con dous elementos {x}, {y}")
+    case (x, y, z):
+        print (f"Coleccion con los elementos {x}, {y}, {z}")
+    case _:
+        print ("Formato desconocido")
