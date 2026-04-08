e84da8e146663d2b3efb6bfcc59d7ccafd477b58
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-04-08 14:44:08 +0200

    Añadido cambios

--- Java/proyectosJava/src/bucles.java
+++ Java/proyectosJava/src/bucles.java
@@ -2,11 +2,21 @@ import java.util.Scanner;
 
 public class bucles {
     public static void main (String[]args){
+        //Bucle while
         int contador = 0;
         while (contador <= 5){
-            System.out.println(contador);
+            System.out.println("Bucle while " + contador);
             contador++;
         }
+
+        //Bucle do-while
+        contador = 0;
+        do{
+            System.out.println("Bucle do-while " + contador);
+            contador++;
+        } while (contador <= 5);
+
+
         Scanner teclado = new Scanner (System.in);
         int opcion = 0;
         while (opcion != 3) {
@@ -16,6 +26,9 @@ public class bucles {
             System.out.println("3.Sair");
             String op = teclado.nextLine();
             opcion = Integer.parseInt(op);
+        }
+        for (int i = 5; i<20; i+=3){
+            System.out.println("Indice: "+i);
         }
     }
 }
